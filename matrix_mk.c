fd066dcb4a6acda3ea66fddf338a227d85ff4fb7
Maanika Khanna <143287646+MaanuG@users.noreply.github.com>
2026-07-17 16:41:20 -0700

    Update matrix_mk.c

--- matrix_mk.c
+++ matrix_mk.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 int main()
 {
     int refmatrix[3][3] = {{3,4,5}, {4,5,6}, {5,6,7}};
@@ -7,24 +8,34 @@ int main()
     int finalmatrix[3][3] = {{3,4,5}, {4,5,6}, {5,6,7}};
     int length=3;
     int sum=0;
-    for (int k=0; k<length; k++)
+    int exponent = 4;
+    for (int e=0; e<exponent-1; e++)
     {
-        for (int j=0; j<length; j++)
+        for (int k=0; k<length; k++)
         {
-            for (int i=0; i<length;i++)
+            for (int j=0; j<length; j++)
             {
-                sum += tempmatrix[k][i] * refmatrix[i][j];
+                for (int i=0; i<length;i++)
+                {
+                    sum += tempmatrix[k][i] * refmatrix[i][j];
+                }
+                finalmatrix[k][j] = sum;
+                sum = 0;
             }
-            finalmatrix[k][j] = sum;
-            sum = 0;
         }
-    }
-    for (int i = 0; i < length; i++) {
-        for (int j = 0; j < length; j++) {
-            printf("%d ", finalmatrix[i][j]); // Print the element and a space
+        printf("Iteration: %i \n", e+2);
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                printf("%d ", finalmatrix[i][j]); // Print the element and a space
+            }
+            printf("\n"); // Move to the next line after finishing a row
         }
-        printf("\n"); // Move to the next line after finishing a row
+        // Copies the number of bytes equivalent to the size of the whole matrix
+        memcpy(refmatrix, finalmatrix, sizeof(finalmatrix));
+
+
     }
+
 
 
     
